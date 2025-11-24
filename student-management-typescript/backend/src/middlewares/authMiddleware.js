5b44df09e346b5538e4d166f8357b350183f9762
Nguyễn Phước Hảo <nguyenphuochao123@gmail.com>
2025-11-24 22:41:15 +0700

    fix: bearer token middleware api

--- student-management-typescript/backend/src/middlewares/authMiddleware.js
+++ student-management-typescript/backend/src/middlewares/authMiddleware.js
@@ -1,14 +1,16 @@
 import jwt from "jsonwebtoken";
 
 const authMiddleware = async (req, res, next) => {
-    const { token } = req.headers;
-
-    if (!token) {
-        return res.status(401).json({ success: false, message: "Authenticate" });
-    }
-
     try {
-        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+        const authHeader = req.headers["authorization"];
+        const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
+
+        if (!token) {
+            return res.status(401).json({ message: "Không tìm thấy access token" });
+        }
+
+        const tokenDecode = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
         if (!tokenDecode) return res.status(401).json({ success: false, message: "Authenticate" });
 
