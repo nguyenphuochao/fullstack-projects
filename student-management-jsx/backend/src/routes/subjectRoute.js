1be457e4cbec922104fe0164efb72343696f6166
Nguyễn Phước Hảo <nguyenphuochao123@gmail.com>
2026-01-08 22:41:45 +0700

    fix: rename project student-management

--- student-management-jsx/backend/src/routes/subjectRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { addSubject, listSubject } from '../controllers/subjectController.js';
-
-const subjectRouter = express.Router();
-
-subjectRouter.get("/list", listSubject);
-subjectRouter.post("/add", addSubject);
-
-export default subjectRouter;
--- student-management-jsx/backend/src/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { getUser } from '../controllers/userController.js';
-
-const userRouter = express.Router();
-
-userRouter.get('/profile', getUser);
-
-export default userRouter;
